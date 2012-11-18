77dc35e2e9683cffe562a162b33481f6d0bdb9e1
Joao Sa <me@joaomsa.com>
2012-11-17 23:20:37 -0200

    Added more table parsing tests.

--- test/table.js
+++ test/table.js
@@ -3,11 +3,13 @@
 var txtwiki = require('../txtwiki.js');
 
 (function(){
+	QUnit.module('Table parsing');
+
 	function parse(then, expected){
 		equal(txtwiki.parseWikitext(then), expected);
 	}
 
-	test('Table parsing', function(){
+	test('Simple tables', function(){
 		parse([
 			'{|',
 			'| A',
@@ -15,12 +17,118 @@ var txtwiki = require('../txtwiki.js');
 			'|-',
 			'| C',
 			'| D',
-			'|}',].join('\n'), '');
+			'|}',].join('\n'),
+			'');
 		parse([
 			'{|',
 			'| A || B',
 			'|-',
 			'| C || D',
-			'|}',].join('\n'), '');
+			'|}',].join('\n'),
+			'');
+		parse([
+			'{| class="Now with some properties" border=\'1\'',
+			'| A',
+			'|-',
+			'| B',
+			'|}',].join('\n'),
+			'');
+		parse([
+			'{| class="wikitable"',
+			'| style="background: #ABCDEF;" | A',
+			'|-',
+			'| formatB',
+			'|}',].join('\n'),
+			'');
+		parse([
+			'{| border="1"',
+			'|-',
+			'|format modifier (not displayed) |These all  |(including the pipes) |go into  |the first cell||second cell',
+			'|-',
+			'|format  |These all  ||format |go into  |the second cell',
+			'|}',].join('\n'),
+			'');
+		parse([
+			'some preceding text {| class="wikitable"',
+			'| A',
+			'|}',].join('\n'),
+			'some preceding text {| class="wikitable" | A |}');
+		parse([ 
+			'                    {| class="wikitable"',
+			'| A',
+			'|}',].join('\n'),
+			'');
+	});
+
+	test('Nested tables', function(){
+		parse([
+			'{| border="1"',
+			'| &alpha;',
+			'| style="text-align: center;"| cell2',
+			'{| border="2" style="background: #ABCDEF;" <!-- The nested table must be on a new line -->',
+			'| NESTED',
+			'|-',
+			'| TABLE',
+			'|}K',
+			'| style="vertical-align:bottom;"| the original table again',
+			'|}',].join('\n'),
+			'');
+	});
+
+	test('Not closed properly', function(){
+		parse([
+			'{| border="1"',
+			'| A',
+			'',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'');
+		parse([
+			'{| border="1"',
+			'| A',
+			'!',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'');
+		parse([
+			'{| border="1"',
+			'|A',
+			'||}',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'');
+		parse([
+			'{| border="1"',
+			'| A',
+			'|-',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'Some text');
+		parse([
+			'{| border="1"',
+			'| A',
+			'|-}',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'Some text');
+		parse([
+			'{| border="1"',
+			'| A',
+			'|+',
+			'',
+			'',
+			'',
+			'Some text',].join('\n'),
+			'Some text');
 	});
 }());
