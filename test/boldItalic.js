2468327f398502fe82a4012e7f9eff2cb522d436
Joao Sa <me@joaomsa.com>
2012-11-17 23:04:37 -0200

    Made tests more readable.

--- test/boldItalic.js
+++ test/boldItalic.js
@@ -3,30 +3,66 @@
 var txtwiki = require('../txtwiki.js');
 
 (function(){
+	QUnit.module('Bold and italic parsing');
+
 	function parse(then, expected){
 		equal(txtwiki.parseWikitext(then), expected);
 	}
 
-	test('Bold and italic parsing', function(){
-		parse("''Italic''", "Italic");
-		parse("'''Bold'''", "Bold");
-		parse("'''''Bold Italic'''''", "Bold Italic");
-		parse("hello ' blah", "hello ' blah");
-		parse("hello '' blah", "hello blah");
-		parse("hello ''' blah", "hello blah");
-		parse("hello l'''amour'' l'''ouest''' blah", "hello l'amour louest blah");
-		parse("hello mon'''amour'' blah", "hello mon'amour blah");
-		parse("hello '''amour'' '''blah '''blah", "hello 'amour blah blah");
-		parse("hello ''''amour''' now ''italics unbalanced, but that's ok", 
+	test('Simple cases', function(){
+		parse(
+			"''Italic''",
+			"Italic");
+		parse(
+			"'''Bold'''",
+			"Bold");
+		parse(
+			"'''''Bold Italic'''''",
+			"Bold Italic");
+		parse(
+			"''Bold '''Italic'' Mismatched'''",
+			"Bold Italic Mismatched");
+	});
+
+	test('Unmatched quotes', function(){
+		parse(
+			"hello ' blah",
+			"hello ' blah");
+		parse(
+			"hello '' blah",
+			"hello blah");
+		parse(
+			"hello ''' blah",
+			"hello blah");
+		parse(
+			"hello l'''amour'' l'''ouest''' blah",
+			"hello l'amour louest blah");
+		parse(
+			"hello mon'''amour'' blah",
+			"hello mon'amour blah");
+		parse(
+			"hello '''amour'' '''blah '''blah",
+			"hello 'amour blah blah");
+		parse(
+			"hello ''''amour''' now ''italics unbalanced, but that's ok",
 			"hello 'amour now italics unbalanced, but that's ok");
-		parse("hello ''''amour''' now, '''bold unbalanced, but that's ok", 
+		parse(
+			"hello ''''amour''' now, '''bold unbalanced, but that's ok",
 			"hello 'amour now, bold unbalanced, but that's ok");
-		parse("hello ''''amour''' now '''''bold and italics unbalanced, so invoke this special case", 
+		parse(
+			"hello ''''amour''' now '''''bold and italics unbalanced, so invoke this special case",
 			"hello ''amour now bold and italics unbalanced, so invoke this special case");
-		parse("hello ''''' blah", "hello blah");
-		parse("hello '''''''''' blah", "hello ''''' blah");
-		parse("hello '''bold '''''''''' blah", "hello bold ''''' blah");
-		parse("He read and reread the ''King James Bible,'' Aesop's ''Fables,'' Bunyan's ''Pilgrim's Progress,'' Defoe's ''Robinson Crusoe,'' and Franklin's ''Autobiography.'''", 
+		parse(
+			"hello ''''' blah",
+			"hello blah");
+		parse(
+			"hello '''''''''' blah",
+			"hello ''''' blah");
+		parse(
+			"hello '''bold '''''''''' blah",
+			"hello bold ''''' blah");
+		parse(
+			"He read and reread the ''King James Bible,'' Aesop's ''Fables,'' Bunyan's ''Pilgrim's Progress,'' Defoe's ''Robinson Crusoe,'' and Franklin's ''Autobiography.'''",
 			"He read and reread the King James Bible, Aesop's Fables, Bunyan's Pilgrim's Progress, Defoe's Robinson Crusoe, and Franklin's Autobiography.'");
 	});
 }());
